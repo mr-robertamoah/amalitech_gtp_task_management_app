f8d90edef514f547bc1ad1233d21b2eb39b1b65e
Robert Amoah <mr_robertamoah@yahoo.com>
2025-05-22 09:22:51 +0000

    Add auth, users, teams, tasks, comments and notifications modules, services and controllers.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TeamsModule } from './teams/teams.module';
+import { CommentsModule } from './comments/comments.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { NotificationController } from './notification/notification.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [AuthModule, UsersModule, TasksModule, TeamsModule, CommentsModule, NotificationsModule],
+  controllers: [AppController, NotificationController],
   providers: [AppService],
 })
 export class AppModule {}
